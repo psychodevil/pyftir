124578a7bef6ef140b226261e89f4bd9477eaf53
psychodevil <s-mahmoud.agha@zewailcity.edu.eg>
2018-12-18 20:49:05 +0200

    Should plot all, but no

--- plotter.py
+++ plotter.py
@@ -1,15 +1,19 @@
 import jcamp
 import matplotlib.pyplot as plt
-filename = 'a.jdx'
-data = jcamp.JCAMP_reader(filename)
-plt.plot(data['x'], data['y'])
-plt.title(filename)
-plt.xlabel(data['xunits'])
-plt.ylabel(data['yunits'])
+import subprocess
 
-jcamp.JCAMP_calc_xsec(data, skip_nonquant=False, debug=False)
-plt.gca().invert_xaxis()
+proc = subprocess.Popen("ls jdx/", stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
+files = out.decode('utf-8').split('\n')[:-1]
+del proc, out, err
+for file_n in files:
+    filename = "./jdx/" + file_n
+    data = jcamp.JCAMP_reader(filename)
+    plt.plot(data['x'], data['y'])
+    plt.title(filename)
+    plt.xlabel(data['xunits'])
+    plt.ylabel(data['yunits'])
+    plt.gca().invert_xaxis()
+    plt.figure()
 plt.show()
-#plt.figure()
-
 
